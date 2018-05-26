2c5046199f0dbcfb39e95be451ebf629cd2a8d1c
gentom <gento.m.developer@gmail.com>
2018-05-26 13:29:14 +0900

    kata 'adding transactions to a block'

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-
+"""
+Blockchain class is responsible for managing the chain.
+It will store transactions and have some helper methods for adding new blocks to the chain.
+"""
 class Blockchain(object):
     def __init__(self):
         self.chain = []
@@ -12,11 +15,32 @@ class Blockchain(object):
         """
         pass
 
+    '''
     def new_transaction(self):
         """
         Adds a new transaction to the list of transactions
         """
         pass
+    '''
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
+
 
     @staticmethod
     def hash(block):
